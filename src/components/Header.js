4a840e3d1aef8a4a3382208377f0713574d3fb27
junhuiz6002 <junhuiz6002@gmail.com>
2023-02-16 19:47:17 -0500

    Tab Color Change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,44 +2,70 @@ import React, { Component } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import { withStyles } from "@material-ui/core";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
 import "./Header.css";
-import Map from "./Map.js";
 
 function Header({ tabValue, setTabValue }) {
   // const [tabValue, setTabValue] = React.useState(0);
+  const [state, setState] = React.useState("");
 
   const handleTabChange = (event, tabValue) => {
     setTabValue(tabValue);
-    // props.state = tabValue;
   };
 
-  const CustomHeaderTab = withStyles({
-    root: {
-      color: "white",
+  const handleStateChange = (event, state) => {
+    setState(state);
+    console.log(state);
+  };
+
+  const styles = {
+    selectedTab: {
+      color: "",
     },
-  })(Tab);
+    nonSelectedTab: {
+      color: "#D3D3D3",
+    },
+  };
 
   return (
     <div>
-      <Box className="HeaderContent" bgcolor="#232023">
+      <Box className="headerContent" bgcolor="#232023">
+        {/* <FormControl className="stateSelector">
+          <InputLabel className="inputLabel">States</InputLabel>
+          <Select value={state} label="State" onChange={handleStateChange}>
+            <MenuItem value={"Oklahoma"}>Oklahoma</MenuItem>
+            <MenuItem value={"Pennsylvania"}>Pennsylvania</MenuItem>
+            <MenuItem value={"Tennessee"}>Tennessee</MenuItem>
+          </Select>
+        </FormControl> */}
+
         <Tabs
           className="Tabs"
           value={tabValue}
           onChange={handleTabChange}
           centered
         >
-          <CustomHeaderTab label="Cumulative Data" />
-          <CustomHeaderTab label="Pennsylvania" />
-          <CustomHeaderTab label="State2" />
-          <CustomHeaderTab label="State3" />
+          <Tab
+            label="Cumulative Data"
+            style={tabValue === 0 ? styles.selectedTab : styles.nonSelectedTab}
+          />
+          <Tab
+            label="States"
+            style={tabValue === 1 ? styles.selectedTab : styles.nonSelectedTab}
+          />
+          <Tab
+            label="Box and Whisker"
+            style={tabValue === 2 ? styles.selectedTab : styles.nonSelectedTab}
+          />
         </Tabs>
       </Box>
 
       {tabValue === 0 && <div> data </div>}
-      {tabValue === 1 && <div> State1 </div>}
-      {tabValue === 2 && <div> State2 </div>}
-      {tabValue === 3 && <div> State3 </div>}
+      {tabValue === 1 && <div> States </div>}
+      {tabValue === 2 && <div> Box and Whisker </div>}
     </div>
   );
 }
