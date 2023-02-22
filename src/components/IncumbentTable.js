1b8cc3056a2f1ed2ec16d5555406f3d07ee91ed9
unknown <kenneth.chin.1@stonybrook.edu>
2023-02-22 00:35:40 -0500

    .

--- src/components/IncumbentTable.js
+++ src/components/IncumbentTable.js
@@ -11,11 +11,11 @@ import tnIncumbent from "./../data/tnIncumbent.json";
 import paIncumbent from "./../data/paIncumbent.json";
 
 
-function IncumbentTable({stateValue}) {
+function IncumbentTable({ stateValue }) {
     const [incumbentData, setIncumbentData] = React.useState([]);
 
     React.useEffect(() => {
-        switch(stateValue) {
+        switch (stateValue) {
             case "":
                 setIncumbentData([]);
             case "pa":
@@ -27,41 +27,41 @@ function IncumbentTable({stateValue}) {
             case "ok":
                 setIncumbentData(okIncumbent.data);
                 break;
-          }
-       
-      });
+        }
+
+    });
     return (
         <>
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-                <TableHead>
-                <TableRow>
-                    <TableCell>District</TableCell>
-                    <TableCell align="right">Name</TableCell>
-                    <TableCell align="right">Party</TableCell>
-                    <TableCell align="right">Election Result&nbsp;(2022)</TableCell>
-                    <TableCell align="right">Geographic Var</TableCell>
-                    <TableCell align="right">Population Var</TableCell>
-                </TableRow>
-                </TableHead>
-                <TableBody>
-                {incumbentData.map((row) => (
-                    // console.log(row)
-                    <TableRow
-                    key={row.District}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                    <TableCell component="th" scope="row">
-                        {row.District}
-                    </TableCell>
-                    <TableCell align="right">{row.Name}</TableCell>
-                    <TableCell align="right">{row.Party}</TableCell>
-                    <TableCell align="right">{row.Win}</TableCell>
-                    </TableRow>
-                ))}
-                </TableBody>
-            </Table>
-    </TableContainer>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>District</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Party</TableCell>
+                            <TableCell align="right">Election Result&nbsp;(2022)</TableCell>
+                            <TableCell align="right">Geographic Var</TableCell>
+                            <TableCell align="right">Population Var</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {incumbentData.map((row) => (
+                            // console.log(row)
+                            <TableRow
+                                key={row.District}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.District}
+                                </TableCell>
+                                <TableCell align="right">{row.Name}</TableCell>
+                                <TableCell align="right">{row.Party}</TableCell>
+                                <TableCell align="right">{row.Win}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </>
     )
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap, Polyline } from "react-leaflet";
 import pa2020 from "./../geoJSON/pa2020.json";
 import pa2022 from "./../geoJSON/pa2022.json";
 import ok2020 from "./../geoJSON/ok2020.json";
@@ -14,6 +14,7 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import FormHelperText from '@mui/material/FormHelperText';
 import Checkbox from '@mui/material/Checkbox';
 import IncumbentTable from "./IncumbentTable"
+import paWinners from "./../data/paWinners.json";
 
 function ChangeView({ center, zoom }) {
   const map = useMap();
@@ -22,24 +23,24 @@ function ChangeView({ center, zoom }) {
   return null;
 }
 
-function Map({tabValue, setTabValue, stateValue, setStateValue}) {
+function Map({ tabValue, setTabValue, stateValue, setStateValue }) {
   const [filter, setFilter] = React.useState({
     twoZero: false,
     twoTwo: true,
     random: false,
   });
- 
+
 
   let center = (stateValue) => {
-    switch(stateValue) {
+    switch (stateValue) {
       case "":
-        return [39.8282,-98.579];
+        return [39.8282, -98.579];
       case "pa":
-        return [41.166113,-77.61552];
+        return [41.166113, -77.61552];
       case "tn":
-        return [35.886,-86.422];
+        return [35.886, -86.422];
       case "ok":
-        return [35.4356,-97.37];
+        return [35.4356, -97.37];
     }
   }
 
@@ -51,78 +52,115 @@ function Map({tabValue, setTabValue, stateValue, setStateValue}) {
   };
 
 
-const district2020 = {
-  color: "white",
-  weight: 1,
-};
+  const district2020 = {
+    color: "white",
+    weight: 1,
+  };
 
-const district2022 = {
-  color: "grey",
-  weight: 1,
-};
+  const district2022 = {
+    color: "grey",
+    weight: 1,
+  };
 
-const { twoZero, twoTwo, random } = filter;
+  let colorDistrict = (feature) => {
+    let district = parseInt(feature.properties.DISTRICT)
+    // console.log(parseInt(feature.properties.DISTRICT), paWinners.data[feature.properties.DISTRICT])
+    if (paWinners.data[district] == "Rep") {
+      return {
+        fillColor: 'red',
+        color: 'black',
+        fillOpacity: 0.6
+
+      }
+    }
+    else if (paWinners.data[district] == "Dem") {
+      return {
+        fillColor: 'blue',
+        color: 'black',
+        fillOpacity: 0.6
+      }
+    }
+    else {
+      return {
+        fillColor: 'grey',
+        color: 'black',
+        fillOpacity: 0.6
+
+      }
+    }
+
+  }
+
+  // fillColor: 'blue',
+  // color: 'lime'
+
+
+  const { twoZero, twoTwo, random } = filter;
 
 
   return (
     <>
-    <Grid container spacing={2}>
-      <Grid item xs={6} md={6}>
-        <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-          <FormLabel component="legend">Select Boundaries</FormLabel>
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Checkbox checked={twoZero} onChange={handleChange} name="twoZero" />
-              }
-              label="2020"
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={6}>
+          <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+            <FormLabel component="legend">Select Boundaries</FormLabel>
+            <FormGroup>
+              <FormControlLabel
+                control={
+                  <Checkbox checked={twoZero} onChange={handleChange} name="twoZero" />
+                }
+                label="2020"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={twoTwo} onChange={handleChange} name="twoTwo" />
+                }
+                label="2022"
+              />
+              <FormControlLabel
+                control={
+                  <Checkbox checked={random} onChange={handleChange} name="random" />
+                }
+                label="Random"
+              />
+            </FormGroup>
+          </FormControl>
+          <IncumbentTable stateValue={stateValue} />
+        </Grid>
+        <Grid item xs={6} md={6}>
+          <MapContainer
+            className="markercluster-map"
+            center={center(stateValue)}
+            zoom={7}
+            maxZoom={10}
+            bounceAtZoomLimits={true}
+            maxBoundsViscosity={1.0}
+            scrollWheelZoom={false}
+          >
+            <ChangeView center={center(stateValue)} zoom={stateValue != "" ? 7 : 4} />
+            <TileLayer
+              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
             />
-            <FormControlLabel
-              control={
-                <Checkbox checked={twoTwo} onChange={handleChange} name="twoTwo" />
-              }
-              label="2022"
-            />
-            <FormControlLabel
-              control={
-                <Checkbox checked={random} onChange={handleChange} name="random" />
-              }
-              label="Random"
-            />
-          </FormGroup>
-        </FormControl>
-        <IncumbentTable stateValue={stateValue}/>
+            {/* {pa2020.features.map((polygon, index) => { return <Polyline key={index} pathOptions={colorDistrict} positions={polygon.geometry.coordinates} /> })} */}
+            {/* {filter.twoZero && <GeoJSON data={pa2020.features} style={district2020} />}
+            {filter.twoTwo && <GeoJSON data={pa2022.features} style={district2022} />} */}
+            {console.log(pa2020.features[0].properties.AREA)}
+            <Polyline pathOptions={colorDistrict} positions={pa2020.features[0].geometry.coordinates[0]} />
+            {filter.twoZero && <GeoJSON data={pa2020.features} style={colorDistrict} />}
+            {filter.twoZero && <GeoJSON data={ok2020.features} style={district2020} />}
+            {filter.twoTwo && <GeoJSON data={ok2022.features} style={district2022} />}
+            {filter.twoZero && <GeoJSON data={tn2020.features} style={district2020} />}
+            {filter.twoTwo && <GeoJSON data={tn2022.features} style={district2022} />}
+          </MapContainer>
+        </Grid>
+
+
       </Grid>
-      <Grid item xs={6} md={6}>
-        <MapContainer
-          className="markercluster-map"
-          center={center(stateValue)}
-          zoom={7}
-          maxZoom={10}
-          bounceAtZoomLimits = {true}
-          maxBoundsViscosity = {1.0}
-          scrollWheelZoom = {false}
-        >
-          <ChangeView center={center(stateValue)} zoom={stateValue != ""? 7 : 4} />
-          <TileLayer
-            url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            
-          />
-          {filter.twoZero && <GeoJSON data={pa2020.features} style={district2020} />}
-          {filter.twoTwo && <GeoJSON data={pa2022.features} style={district2022} />}
-          {filter.twoZero && <GeoJSON data={ok2020.features} style={district2020} />}
-          {filter.twoTwo && <GeoJSON data={ok2022.features} style={district2022} />}
-          {filter.twoZero && <GeoJSON data={tn2020.features} style={district2020} />}
-          {filter.twoTwo && <GeoJSON data={tn2022.features} style={district2022} />}
-        </MapContainer>
-      </Grid>
-    
-      
-    </Grid>
-    
-      
-  
+
+
+
     </>
   );
 }
