9cb7237bc67b1de588a4ee7400e05dc1f01cdef9
junhuiz6002 <junhuiz6002@gmail.com>
2023-02-22 04:56:56 -0500

    Restructure Files

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
-import { MapContainer, TileLayer, Marker, Popup, GeoJSON, useMap, Polyline } from "react-leaflet";
+import React from "react";
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  useMap,
+  Polyline,
+} from "react-leaflet";
 import pa2020 from "./../geoJSON/pa2020.json";
 import pa2022 from "./../geoJSON/pa2022.json";
 import ok2020 from "./../geoJSON/ok2020.json";
 import ok2022 from "./../geoJSON/ok2022.json";
 import tn2020 from "./../geoJSON/tn2020.json";
 import tn2022 from "./../geoJSON/tn2022.json";
-import Grid from "@mui/material/Grid";
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormHelperText from '@mui/material/FormHelperText';
-import Checkbox from '@mui/material/Checkbox';
-import IncumbentTable from "./IncumbentTable"
 import paWinners from "./../data/paWinners.json";
 
 function ChangeView({ center, zoom }) {
@@ -23,14 +21,7 @@ function ChangeView({ center, zoom }) {
   return null;
 }
 
-function Map({ tabValue, setTabValue, stateValue, setStateValue }) {
-  const [filter, setFilter] = React.useState({
-    twoZero: false,
-    twoTwo: true,
-    random: false,
-  });
-
-
+function Map({ stateValue, filter }) {
   let center = (stateValue) => {
     switch (stateValue) {
       case "":
@@ -41,16 +32,10 @@ function Map({ tabValue, setTabValue, stateValue, setStateValue }) {
         return [35.886, -86.422];
       case "ok":
         return [35.4356, -97.37];
+      default:
+        return [0, 0];
     }
-  }
-
-  const handleChange = (event) => {
-    setFilter({
-      ...filter,
-      [event.target.name]: event.target.checked,
-    });
   };
-
 
   const district2020 = {
     color: "white",
@@ -63,104 +48,77 @@ function Map({ tabValue, setTabValue, stateValue, setStateValue }) {
   };
 
   let colorDistrict = (feature) => {
-    let district = parseInt(feature.properties.DISTRICT)
+    let district = parseInt(feature.properties.DISTRICT);
     // console.log(parseInt(feature.properties.DISTRICT), paWinners.data[feature.properties.DISTRICT])
-    if (paWinners.data[district] == "Rep") {
+    if (paWinners.data[district] === "Rep") {
       return {
-        fillColor: 'red',
-        color: 'black',
-        fillOpacity: 0.6
-
-      }
-    }
-    else if (paWinners.data[district] == "Dem") {
+        fillColor: "red",
+        color: "black",
+        fillOpacity: 0.6,
+      };
+    } else if (paWinners.data[district] === "Dem") {
       return {
-        fillColor: 'blue',
-        color: 'black',
-        fillOpacity: 0.6
-      }
-    }
-    else {
+        fillColor: "blue",
+        color: "black",
+        fillOpacity: 0.6,
+      };
+    } else {
       return {
-        fillColor: 'grey',
-        color: 'black',
-        fillOpacity: 0.6
-
-      }
+        fillColor: "grey",
+        color: "black",
+        fillOpacity: 0.6,
+      };
     }
-
-  }
+  };
 
   // fillColor: 'blue',
   // color: 'lime'
 
-
-  const { twoZero, twoTwo, random } = filter;
-
+  // const { twoZero, twoTwo, random } = filter;
 
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={6} md={6}>
-          <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
-            <FormLabel component="legend">Select Boundaries</FormLabel>
-            <FormGroup>
-              <FormControlLabel
-                control={
-                  <Checkbox checked={twoZero} onChange={handleChange} name="twoZero" />
-                }
-                label="2020"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox checked={twoTwo} onChange={handleChange} name="twoTwo" />
-                }
-                label="2022"
-              />
-              <FormControlLabel
-                control={
-                  <Checkbox checked={random} onChange={handleChange} name="random" />
-                }
-                label="Random"
-              />
-            </FormGroup>
-          </FormControl>
-          <IncumbentTable stateValue={stateValue} />
-        </Grid>
-        <Grid item xs={6} md={6}>
-          <MapContainer
-            className="markercluster-map"
-            center={center(stateValue)}
-            zoom={7}
-            maxZoom={10}
-            bounceAtZoomLimits={true}
-            maxBoundsViscosity={1.0}
-            scrollWheelZoom={false}
-          >
-            <ChangeView center={center(stateValue)} zoom={stateValue != "" ? 7 : 4} />
-            <TileLayer
-              url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-
-            />
-            {/* {pa2020.features.map((polygon, index) => { return <Polyline key={index} pathOptions={colorDistrict} positions={polygon.geometry.coordinates} /> })} */}
-            {/* {filter.twoZero && <GeoJSON data={pa2020.features} style={district2020} />}
+      <MapContainer
+        className="markercluster-map"
+        center={center(stateValue)}
+        zoom={7}
+        maxZoom={10}
+        bounceAtZoomLimits={true}
+        maxBoundsViscosity={1.0}
+        scrollWheelZoom={false}
+      >
+        <ChangeView
+          center={center(stateValue)}
+          zoom={stateValue !== "" ? 7 : 4}
+        />
+        <TileLayer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {/* {pa2020.features.map((polygon, index) => { return <Polyline key={index} pathOptions={colorDistrict} positions={polygon.geometry.coordinates} /> })} */}
+        {/* {filter.twoZero && <GeoJSON data={pa2020.features} style={district2020} />}
             {filter.twoTwo && <GeoJSON data={pa2022.features} style={district2022} />} */}
-            {console.log(pa2020.features[0].properties.AREA)}
-            <Polyline pathOptions={colorDistrict} positions={pa2020.features[0].geometry.coordinates[0]} />
-            {filter.twoZero && <GeoJSON data={pa2020.features} style={colorDistrict} />}
-            {filter.twoZero && <GeoJSON data={ok2020.features} style={district2020} />}
-            {filter.twoTwo && <GeoJSON data={ok2022.features} style={district2022} />}
-            {filter.twoZero && <GeoJSON data={tn2020.features} style={district2020} />}
-            {filter.twoTwo && <GeoJSON data={tn2022.features} style={district2022} />}
-          </MapContainer>
-        </Grid>
-
-
-      </Grid>
-
-
-
+        {/* {console.log(pa2020.features[0].properties.AREA)} */}
+        <Polyline
+          pathOptions={colorDistrict}
+          positions={pa2020.features[0].geometry.coordinates[0]}
+        />
+        {filter.twoZero && (
+          <GeoJSON data={pa2020.features} style={colorDistrict} />
+        )}
+        {filter.twoZero && (
+          <GeoJSON data={ok2020.features} style={district2020} />
+        )}
+        {filter.twoTwo && (
+          <GeoJSON data={ok2022.features} style={district2022} />
+        )}
+        {filter.twoZero && (
+          <GeoJSON data={tn2020.features} style={district2020} />
+        )}
+        {filter.twoTwo && (
+          <GeoJSON data={tn2022.features} style={district2022} />
+        )}
+      </MapContainer>
     </>
   );
 }
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import Map from "./Map.js";
 import Header from "./Header.js";
+import Data from "./Data.js";
+import Grid from "@mui/material/Grid";
+import Accordion from "@mui/material/Accordion";
 
 function Project() {
   const [tabValue, setTabValue] = React.useState(0);
   const [stateValue, setStateValue] = React.useState("");
-  // console.log(tabValue)
+  const [filter, setFilter] = React.useState({
+    twoZero: false,
+    twoTwo: true,
+    random: false,
+  });
+
+  const { twoZero, twoTwo, random } = filter;
 
   return (
     <div>
@@ -15,12 +24,25 @@ function Project() {
         stateValue={stateValue}
         setStateValue={setStateValue}
       />
-      <Map
-        tabValue={tabValue}
-        setTabValue={setTabValue}
-        stateValue={stateValue}
-        setStateValue={setStateValue}
-      />
+      {tabValue === 1 ? (
+        <Grid container spacing={1}>
+          <Grid item xs={6} md={6}>
+            <Data
+              filter={filter}
+              setFilter={setFilter}
+              twoZero={twoZero}
+              twoTwo={twoTwo}
+              random={random}
+              stateValue={stateValue}
+            />
+          </Grid>
+          <Grid item xs={6} md={6}>
+            <Map stateValue={stateValue} filter={filter} />
+          </Grid>
+        </Grid>
+      ) : (
+        <span />
+      )}
     </div>
   );
 }
