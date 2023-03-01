fb4ede42ff69e764904821bccfa649a37d4464ed
unknown <kenneth.chin.1@stonybrook.edu>
2023-03-01 00:13:38 -0500

    data

--- src/components/DistrictPlanSummary.js
+++ src/components/DistrictPlanSummary.js
@@ -1,15 +1,14 @@
 import React from "react";
 import {
-  TableContainer,
-  Paper,
-  TableHead,
-  TableRow,
-  TableBody,
-  Table,
-  TableCell,
+    TableContainer,
+    Paper,
+    TableHead,
+    TableRow,
+    TableBody,
+    Table,
+    TableCell,
 } from "@mui/material";
 function DistrictPlanSummary() {
-<<<<<<< HEAD
 
     const [districtIncumbentData, setDistrictIncumbentData] = React.useState([
         {
@@ -71,39 +70,6 @@ function DistrictPlanSummary() {
         </TableContainer>
 
     </>)
-=======
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Number of Districts: {10}</TableCell>
-              {/* <TableCell>10</TableCell> */}
-            </TableRow>
-          </TableHead>
-          <TableHead>
-            <TableRow>
-              <TableCell align="left">Incumbent</TableCell>
-              <TableCell align="left">Party</TableCell>
-              <TableCell align="left">Incumbent District Variation</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow
-              // key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell align="left">{"John Doe"}</TableCell>
-              <TableCell align="left">{"Republican"}</TableCell>
-              <TableCell align="left">{3}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
-  );
->>>>>>> c1dd47af2513757dcd5b4fb5d714b98eff560eb0
 }
 
 export default DistrictPlanSummary;
