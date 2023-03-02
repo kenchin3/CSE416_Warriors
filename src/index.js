8ea4ffb824a8a7b48372789da45c11991662137e
junhuiz6002 <junhuiz6002@gmail.com>
2023-03-01 19:41:03 -0500

    Fix Merge Conflicts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
-import "./components/IncumbentTable.css"
 import App from "./App";
 
 const root = document.getElementById("root");
