b9f6d34458b5184adb5c923bd39c4b1e09990494
Jeffrey Zhang <78513345+junhuiz6002@users.noreply.github.com>
2023-04-24 05:51:39 -0400

    Combining with prepro files

---
+++ prepro/neighbors.py
@@ -0,0 +1,28 @@
+import libpysal as lps
+import geopandas as gpd
+import csv
+
+
+state = "ok"
+file = ("./data/ok_boundary.json")
+shp = gpd.read_file(file)
+
+rW = lps.weights.Rook.from_dataframe(shp, idVariable="GEOID")
+
+outputName = "./out/" + state + "_neighbors.csv"
+header = ['id','NEIGHBORS']
+
+with open(outputName, 'w', newline='') as csv_out:
+    writeCSV = csv.writer(csv_out, delimiter=',')
+    writeCSV.writerow(header)
+    for row in rW:
+        id = row[0]
+        neighbors = row[1]
+        neighborIDs = list(neighbors.keys())
+        n = []
+        for i in neighborIDs:
+                n.append(i)
+        neighborString = ",".join(n)
+        writeCSV.writerow([row[0], neighborString])
+csv_out.close() 
+
