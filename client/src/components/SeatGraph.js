dc53a6e81ed2fff41d21b9886ead15ab26a36712
junhuiz6002 <junhuiz6001@gmail.com>
2023-04-25 02:18:39 -0400

    Single Row SeatGraph

--- client/src/components/SeatGraph.js
+++ client/src/components/SeatGraph.js
@@ -1,14 +1,8 @@
 import React from "react";
-import okIncumbent from "./../data/okIncumbent.json";
-import tnIncumbent from "./../data/tnIncumbent.json";
-import paIncumbent from "./../data/paIncumbent.json";
-import "./SeatGraph.css";
-import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from "recharts";
-import { Typography } from "@mui/material";
+import ReactApexChart from "react-apexcharts";
 
 function SeatGraph({ stateValue, incumbentData }) {
   function getData() {
-   
     let arr = [0, 0, 0];
     incumbentData.forEach((element) => {
       if (element.electionResult === "Open") {
@@ -19,40 +13,79 @@ function SeatGraph({ stateValue, incumbentData }) {
         arr[0] += 1;
       }
     });
-    return [
-      { name: "Democrat ", value: arr[0], fill: "blue" },
-      { name: "Republican ", value: arr[1], fill: "red" },
-      { name: "Open ", value: arr[2], fill: "grey" },
+    let res = [
+      {
+        name: "Democrat",
+        data: [arr[0]],
+      },
+      {
+        name: "Republican",
+        data: [arr[1]],
+      },
+      {
+        name: "Open",
+        data: [arr[2]],
+      },
     ];
+    return res;
   }
+
+  const seatGraphOptions = {
+    chart: {
+      type: "bar",
+      // height: 120,
+      stacked: true,
+      toolbar: {
+        show: false,
+      },
+      sparkline: {
+        enabled: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+        dataLabels: {
+          total: {
+            enabled: true,
+            offsetX: 0,
+            style: {
+              fontSize: "11px",
+              fontWeight: 700,
+            },
+          },
+        },
+      },
+    },
+    stroke: {
+      width: 1,
+    },
+    xaxis: {
+      categories: [stateValue.toUpperCase()],
+      labels: {
+        formatter: function (val) {
+          return val;
+        },
+      },
+    },
+    fill: {
+      opacity: 1,
+    },
+    legend: {
+      position: "bottom",
+      horizontalAlign: "left",
+      offsetX: 30,
+    },
+  };
 
   return (
     <>
-      <ResponsiveContainer width="100%" height={125}>
-        <BarChart
-          data={getData()}
-          layout="vertical"
-          barCategoryGap={3}
-          margin={{ top: 0, right: 50, left: 0, bottom: 0 }}
-        >
-          <XAxis type="number" />
-          <YAxis
-            type="category"
-            width={150}
-            padding={{ left: 30 }}
-            dataKey="name"
-          />
-
-          <Bar dataKey="value" fill="fill" radius={[0, 3, 3, 0]} />
-        </BarChart>
-      </ResponsiveContainer>
-      <Typography className="summaryInformation"></Typography>
-      <div className="summaryInformationLocaiton">
-        <div className="summaryInformationFont">
-          <span style={{ fontWeight: 550 }}> Number of Districts: </span>{" "}
-          {incumbentData.length}
-        </div>
-      </div>
+      <ReactApexChart
+        options={seatGraphOptions}
+        series={getData()}
+        type="bar"
+        height="110"
+      />
     </>
   );
 }
